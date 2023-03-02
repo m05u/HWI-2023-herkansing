c83dafd25652c0d57059cd5e44cc48ec9a562446
Moustapha Azaimi <m.azaimi@student.avans.nl>
2023-03-02 12:33:55 +0100

    CREATED basic rgb led
    week 2 boebot opdracht 3a

--- BoeBotProject/src/RobotMain.java
+++ BoeBotProject/src/RobotMain.java
@@ -1,10 +1,20 @@
 import TI.BoeBot;
+import TI.PWM;
 import TI.PinMode;
 import Week2.Week2;
 
 public class RobotMain {
 
     public static void main(String[] args) {
-        Week2.opdrachtZesA();
+        BoeBot.setMode(0, PinMode.Output); // red
+        BoeBot.setMode(1, PinMode.Output); // green
+        BoeBot.setMode(2, PinMode.Output); // blue
+
+        boolean on = false;
+        boolean off = true;
+
+        BoeBot.digitalWrite(0, on);
+        BoeBot.digitalWrite(1, off);
+        BoeBot.digitalWrite(2, off);
     }
 }
